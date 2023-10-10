164d431c59b07ea51fc61212753435f39c3a466f
karem880 <elalamymee@gmail.com>
2023-10-10 19:12:19 +0200

    --

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { FaBook, FaStar,FaQuestion, FaBars,FaCarAlt } from 'react-icons/fa'; // Example using Font Awesome icons
+import { FaBook, FaStar,FaQuestion, FaBars,FaCarAlt,FaUser,FaHeart } from 'react-icons/fa'; // Example using Font Awesome icons
 import { MdShoppingCart } from 'react-icons/md';
 function NavBar() {
     const [showMenu, setShowMenu] = useState(false);
@@ -15,30 +15,78 @@ function NavBar() {
         <>
 
 
-<header className=' hidden md:flex justify-around p-5  w-full fixed top-0 left-0 z-50  bg-slate-700 text-green-400   items-center drop-shadow-2xl '>
-<p className='text-lg font-extrabold'>Who we are</p>
-            <h1 className='flex  items-center justify-between text-lg md:text-2xl font-extrabold  '><MdShoppingCart className='md:mr-5'/>Marketko</h1>
-            <div className='flex flex-col justify-between items-center '>
-                <FaQuestion/>
-                <p className='text-lg font-extrabold'>Help</p>
+<header className=' hidden md:flex justify-between p-5  w-full fixed top-0 left-0 z-50  uppercase text-lg font-bold  items-center drop-shadow-2xl px-10 bg-white shadow-gray-400 h-[70px] overflow-hidden '>
+  <div>
+    <img src="/public/assets/loogo.webp" width={100} className='object-cover' alt="" />
+  </div>
+  <div>
+    <ul className='flex items-center justify-center gap-10'>
+      <li>home</li>
+      <li>alll</li>
+      <li>category</li>
+      <li>contact</li>
+      <li>aboutdev</li>
+    </ul>
+  </div>
+  <div>
+    <ul className='flex items-center justify-center gap-10'>
+      <li ><FaUser size={24}/>
+      </li>
+      <li className='relative'><FaHeart size={24} />
+      <span className='flex absolute top-[-18px] right-[-10px] w-[20px] h-[20px] rounded-full bg-green-400 text-white items-center justify-center'>0</span>
+      </li>
+      <li className='relative'><MdShoppingCart size={24}/>
+      <span className='flex absolute top-[-18px] right-[-10px] w-[20px] h-[20px] rounded-full bg-green-400 text-white items-center justify-center'>0</span>
+</li>
+    </ul>
+  </div>
 
-            </div>
 
  </header>
-<nav className=' w-full fixed top-0 left-0 bg-slate-700 text-green-400  z-50'>
+
+
+
+
+
+
+
+
+
+
+
+
+<nav className=' w-full fixed top-0 left-0 bg-white text-xl font-bold  z-50 '>
     <header className='md:hidden flex justify-between p-5    items-center drop-shadow-2xl relative'>
-    <h1 className='flex  items-center justify-between text-lg md:text-2xl font-extrabold  '><MdShoppingCart className='md:mr-5'/>Marketko</h1>
+    <img src="/public/assets/loogo.webp" width={100} className='object-cover' alt="" />
+    <div>
+    <ul className='flex items-center justify-center gap-10'>
+    <li ><FaUser size={24}/>
+      </li>
+      <li className='relative'><FaHeart size={24} />
+      <span className='flex absolute top-[-18px] right-[-10px] w-[20px] h-[20px] rounded-full bg-green-400 text-white items-center justify-center'>0</span>
+      </li>
+      <li className='relative'><MdShoppingCart size={24}/>
+      <span className='flex absolute top-[-18px] right-[-10px] w-[20px] h-[20px] rounded-full bg-green-400 text-white items-center justify-center'>0</span>
+</li>
+    </ul>
+  </div>
       <p className='cursor-pointer ' onClick={toggleMenu}>
+        
         <FaBars className='text-green-500 dark:text-white dark:hover:text-gray-300' size={22} />
       </p>
-      {showMenu &&(
-         <ul className="absolute bottom-[-144px]  h-[150px] flex flex-col justify-around items-start text-xl left-0 w-full p-5  bg-slate-500  z-50" >
-       <p className='text-lg font-extrabold'>Who we are</p>
-       <div className='flex  justify-between items-center '>
-                <FaQuestion />
-                <p className='text-lg font-extrabold'>Help</p>
 
-            </div>
+      {showMenu &&(
+         <ul className="absolute bottom-[-184px]  h-[200px] flex flex-col justify-center gap-5 items-start text-xl left-0 w-full p-5  text-white bg-green-500  z-50" >
+ 
+  <div>
+    <ul className=''>
+      <li>home</li>
+      <li>alll</li>
+      <li>category</li>
+      <li>contact</li>
+      <li>aboutdev</li>
+    </ul>
+  </div>
      
         
        </ul>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from '../component/NavBar';
 import axios from 'axios';
+import { MdShoppingCart } from 'react-icons/md';
+
+import Slider from 'react-slick';
+import ComplexTouchGallery from './slider';
+import CustomSlider from './slider';
+import CustomMarketSlider from './slider';
+import CustomSwiperSlider from './slider';
 
 function Home() {
   const [data,setdata] =useState([])
@@ -56,93 +63,93 @@ function Home() {
 
 
 
-  const vegtoggle=()=>{
-    setvegstates(!vegstates)
-    setfruitsstates(false)
-    setcheesestates(false)
-    setdaiarystates(false)
-    setmeatstates(false)
-    setdessertsstates(false)
-    setbeveragesTeastate(false)
+  // const vegtoggle=()=>{
+  //   setvegstates(!vegstates)
+  //   setfruitsstates(false)
+  //   setcheesestates(false)
+  //   setdaiarystates(false)
+  //   setmeatstates(false)
+  //   setdessertsstates(false)
+  //   setbeveragesTeastate(false)
 
 
-  }
-  const fruitstoggle=()=>{
-    setfruitsstates(!fruitsstates)
-    setvegstates(false)
-    setcheesestates(false)
-    setdaiarystates(false)
-    setmeatstates(false)
-    setdessertsstates(false)
-    setbeveragesTeastate(false)
-
-
-
-  }
-  const cheesetoggle=()=>{
-    setcheesestates(!cheesestates)
-    setvegstates(false)
-    setfruitsstates(false)
-    setdaiarystates(false)
-    setmeatstates(false)
-    setdessertsstates(false)
-    setbeveragesTeastate(false)
+  // }
+  // const fruitstoggle=()=>{
+  //   setfruitsstates(!fruitsstates)
+  //   setvegstates(false)
+  //   setcheesestates(false)
+  //   setdaiarystates(false)
+  //   setmeatstates(false)
+  //   setdessertsstates(false)
+  //   setbeveragesTeastate(false)
 
 
 
-
-  }
-  const daiarytoggle=()=>{
-    setdaiarystates(!daiarystates)
-    setvegstates(false)
-    setfruitsstates(false)
-    setcheesestates(false)
-    setmeatstates(false)
-    setdessertsstates(false)
-    setbeveragesTeastate(false)
-
-
-
-  }
-  const meattoggle=()=>{
-    setmeatstates(!meatstates)
-    setvegstates(false)
-    setfruitsstates(false)
-    setdaiarystates(false)
-    setcheesestates(false)
-    setdessertsstates(false)
-    setbeveragesTeastate(false)
-
-
-  }
-
-
-  const dessertstoggle=()=>{
-    setdessertsstates(!dessertsstates)
-    setvegstates(false)
-    setfruitsstates(false)
-    setdaiarystates(false)
-    setcheesestates(false)
-    setmeatstates(false)
-    setbeveragesTeastate(false)
+  // }
+  // const cheesetoggle=()=>{
+  //   setcheesestates(!cheesestates)
+  //   setvegstates(false)
+  //   setfruitsstates(false)
+  //   setdaiarystates(false)
+  //   setmeatstates(false)
+  //   setdessertsstates(false)
+  //   setbeveragesTeastate(false)
 
 
 
-  }
+
+  // }
+  // const daiarytoggle=()=>{
+  //   setdaiarystates(!daiarystates)
+  //   setvegstates(false)
+  //   setfruitsstates(false)
+  //   setcheesestates(false)
+  //   setmeatstates(false)
+  //   setdessertsstates(false)
+  //   setbeveragesTeastate(false)
 
 
-  const teatoggle=()=>{
-    setbeveragesTeastate(!beveragesTeastate)
-    setvegstates(false)
-    setfruitsstates(false)
-    setdaiarystates(false)
-    setcheesestates(false)
-    setmeatstates(false)
-    setdessertsstates(false)
+
+  // }
+  // const meattoggle=()=>{
+  //   setmeatstates(!meatstates)
+  //   setvegstates(false)
+  //   setfruitsstates(false)
+  //   setdaiarystates(false)
+  //   setcheesestates(false)
+  //   setdessertsstates(false)
+  //   setbeveragesTeastate(false)
+
+
+  // }
+
+
+  // const dessertstoggle=()=>{
+  //   setdessertsstates(!dessertsstates)
+  //   setvegstates(false)
+  //   setfruitsstates(false)
+  //   setdaiarystates(false)
+  //   setcheesestates(false)
+  //   setmeatstates(false)
+  //   setbeveragesTeastate(false)
 
 
 
-  }
+  // }
+
+
+  // const teatoggle=()=>{
+  //   setbeveragesTeastate(!beveragesTeastate)
+  //   setvegstates(false)
+  //   setfruitsstates(false)
+  //   setdaiarystates(false)
+  //   setcheesestates(false)
+  //   setmeatstates(false)
+  //   setdessertsstates(false)
+
+
+
+  // }
 
   
 
@@ -152,7 +159,104 @@ function Home() {
     <>
       <NavBar />
 
-      <div className='w-full flex justify-center gap-10 flex-col md:flex-row  mt-52 flex-wrap '>
+
+
+      <section className='flex flex-col md:flex-row gap-20 md:gap-3 w-full items-center justify-around text-4xl mt-16 bg-green-50 p-8 text-center'>
+    <div className='p-5 flex items-center flex-col h-full mt-10'>
+        <h1 className='text-green-600 text-3xl font-extrabold uppercase mb-4'>Welcome to My Market</h1>
+        <h2 className='text-green-600 mt-2 text-lg font-extrabold text-center'>Explore our sections by clicking the button below.</h2>
+
+        <p className='text-gray-700 text-base mt-4'>Discover the freshest produce and more!</p>
+        <button className='w-full md:w-[70%] text-xl font-bold p-3 bg-green-600 text-white flex items-center justify-center h-[50px] rounded-lg mt-6 hover:bg-green-700 transition duration-300 ease-in-out'>Explore Market</button>
+    </div>
+
+    <div className='flex items-center justify-center h-full flex-col md:flex-row w-full md:w-[50%] mt-10'>
+        <div className='flex h-[70%] flex-col md:flex-row overflow-hidden gap-3 rounded-2xl'>
+            <div className='w-full md:w-[50%] h-full flex flex-col gap-3'>
+                <img src="/public/assets/vegtablesheader.webp" className='h-[300px] rounded-lg object-cover w-full md:w-[300px]' alt="" />
+                <img src="/public/assets/grid1.jpg" className='h-[300px] rounded-lg object-cover w-full md:w-[300px]' alt="" />
+            </div>
+
+            <div className='w-full md:w-[50%] h-full flex flex-col gap-3'>
+                <img src="/public/assets/grid3.webp" className='h-[300px] rounded-lg object-cover w-full md:w-[300px]' alt="" />
+                <img src="/public/assets/grid4.webp" className='h-[300px] rounded-lg object-cover w-full md:w-[300px]' alt="" />
+            </div>
+        </div>
+    </div>
+</section>
+<ComplexTouchGallery />
+
+
+
+<section className='relative flex flex-col md:flex-row gap-10 w-full h-[80vh] mt-44'>
+  <div className='w-full md:w-[70%] h-[100%]'>
+    <div className='h-full bg-cover relative border-dashed border border-green-500 flex-col rounded-2xl bg-current flex items-center  justify-center py-5 text-2xl font-extrabold text-green-600' style={{ backgroundImage: 'url("/public/assets/Home.png")' }}>
+      {/* Content for the first div */}
+      <p>Discover the Hidden Treasures</p>
+      <p className="text-xl font-semibold mt-4">Explore our exquisite collection of fresh and premium products.</p>
+      <button className='w-[70%] md:w-[50%] text-xl font-bold p-3 bg-green-600 text-white flex items-center justify-center h-[50px] rounded-lg mt-6 hover:bg-green-700 transition duration-300 ease-in-out'>Explore Market</button>
+
+    </div>
+  </div>
+
+  <div className='w-full md:w-[30%] flex flex-col gap-5 h-full '>
+   
+    
+    <div className='h-[50%] bg-cover relative text-center  border-dashed border border-green-500 flex-col rounded-2xl bg-current flex items-center  justify-center py-5 text-2xl font-extrabold text-green-600' style={{ backgroundImage: 'url("/public/assets/onion.png")' }}>
+      {/* Content for the first div */}
+      <p>Discover the Hidden Treasures</p>
+      <p className="text-xl font-semibold mt-4">Explore our exquisite collection of fresh and premium products.</p>
+      <button className=' absolute top-[-40px] left-[-10px] rounded-full  text-xl font-bold p-3 bg-yellow-400 w-[50px] h-[50px] text-white flex items-center justify-center   mt-6 hover:bg-yellow-700 transition duration-300 ease-in-out'>-50%</button>
+
+    </div>
+
+
+
+    <div className='h-[50%] bg-cover relative border-dashed border text-center  border-green-500 flex-col rounded-2xl bg-current flex items-center  justify-center py-5 text-2xl font-extrabold text-green-600' style={{ backgroundImage: 'url("/public/assets/felfel.png")' }}>
+      {/* Content for the first div */}
+      <p>Discover the Hidden Treasures</p>
+      <p className="text-xl font-semibold mt-4">Explore our exquisite collection of fresh and premium products.</p>
+      <button className=' absolute top-[-40px] left-[-10px] rounded-full  text-xl font-bold p-3 bg-green-600 w-[50px] h-[50px] text-white flex items-center justify-center   mt-6 hover:bg-green-700 transition duration-300 ease-in-out'>-50%</button>
+ 
+    </div>
+
+
+
+  </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* Creative Section */}
+<section className='bg-gradient-to-r from-green-500 to-green-300 py-24 mt-32 text-center text-white'>
+  <div className='container mx-auto flex flex-col items-center'>
+    <div className='w-full flex items-center justify-center text-white text-[100px] py-10'>
+      <MdShoppingCart/>
+    </div>
+    <h2 className='text-4xl font-extrabold mb-6'>Discover Something Extraordinary</h2>
+    <p className='text-2xl mb-10'>Unleash your imagination and embark on a journey of flavor and delight with our unique selection of products.</p>
+    <Link to='/' className='bg-gradient-to-r from-purple-500 to-red-300 hover:bg-yellow-600 text-white text-lg font-semibold px-8 py-4 rounded-full transition duration-300 ease-in-out'>Explore Now</Link>
+  </div>
+</section>
+
+
+
+      {/* <div className='w-full flex justify-center gap-10 flex-col md:flex-row  mt-52 flex-wrap '>
         <button onClick={vegtoggle} className=' w-[80] md:w-[20%] rounded-xl bg-green-600 text-white flex items-center justify-center h-[60px] hover:scale-125 duration-1000 hover:bg-green-400'>show vegtables</button>
         <button onClick={fruitstoggle} className=' w-[80] md:w-[20%] rounded-xl bg-red-600 text-white flex items-center justify-center h-[60px] hover:scale-125 duration-1000 hover:bg-red-400'>show fruits</button>
         <button onClick={cheesetoggle} className=' w-[80] md:w-[20%] rounded-xl bg-blue-600 text-white flex items-center justify-center h-[60px] hover:scale-125 duration-1000 hover:bg-blue-400'>show cheese</button>
@@ -266,7 +370,7 @@ function Home() {
               </div>
             ))}
           </div>
-      ):null}
+      ):null} */}
 
 
 
